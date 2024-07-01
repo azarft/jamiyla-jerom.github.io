f8609410684eb6616e509d69eec81ae66bfd4da3
azarft <argoazanov@gmail.com>
2024-07-02 00:28:23 +0600

    First demo of site

--- js/main.js
+++ js/main.js
@@ -212,5 +212,21 @@
 		counterWayPoint();
 	});
 
+	document.addEventListener("DOMContentLoaded", function() {
+		var video = document.getElementById("weddingVideo");
+	  
+		var observer = new IntersectionObserver(function(entries) {
+			entries.forEach(function(entry) {
+				if (entry.isIntersecting) {
+					video.play();
+				} else {
+					video.pause();
+				}
+			});
+		}, { threshold: 0.5 });
+	  
+		observer.observe(video);
+	  });
+
 
 }());
---
+++ js/video.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("weddingVideo");
+
+    var observer = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                video.play();
+            } else {
+                video.pause();
+            }
+        });
+    }, { threshold: 0.5 });
+
+    observer.observe(video);
+});
